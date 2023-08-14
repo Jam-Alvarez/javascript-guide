d03229bc72a1318ddd3bdbaf83d2cc8d4689c800
Jam-Alvarez <bc.jamiraquaimikhail.alvarez@cvsu.edu.ph>
2023-08-15 01:12:07 +0800

    03 Arithmetic Expressions Added

---
+++ root/03-arithmetic-expressions/index.js
@@ -0,0 +1,37 @@
+
+/* 
+    arithmetic expression is a combination of...
+    operand (values, variables, etc.)
+    operators ( +, -, *, /, %)
+    that can be evaluated to a value
+    example: y = x +5;
+*/
+
+let students = 20;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+
+// let extraStudents = students % 3;
+
+// shortcut for above, only use this if your re-assigning the same variable like above
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division
+    4. addition & subtraction
+*/
+
+let result = (1 + 2) * (3 + 4);
+
+console.log(result);
+
+
