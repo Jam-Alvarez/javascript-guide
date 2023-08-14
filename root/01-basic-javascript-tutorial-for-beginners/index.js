c6d9d08ed91c331e28e9d329750fe2e7c90e8427
Jam-Alvarez <bc.jamiraquaimikhail.alvarez@cvsu.edu.ph>
2023-08-15 01:05:35 +0800

    01 Basic JavaScript Added

---
+++ root/01-basic-javascript-tutorial-for-beginners/index.js
@@ -0,0 +1,14 @@
+
+console.log('Hi, my name is Moonbami');
+console.log("and this is my JavaScript guide for begginers");
+alert('ARE YOU READY?!');
+
+// this is a single line comment
+
+/*
+    this 
+    is 
+    a 
+    multi-line
+    comment
+*/
