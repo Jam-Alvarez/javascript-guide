5bf09735aa3741ab01c59ef882a5d567de56af96
Jam-Alvarez <bc.jamiraquaimikhail.alvarez@cvsu.edu.ph>
2023-08-18 09:46:41 +0800

    Fixed Comments

--- root/34-sort-an-array-of-strings/index.js
+++ root/34-sort-an-array-of-strings/index.js
@@ -1,9 +1,10 @@
 
-// this time, let's sort some an array of strings in javascript
+// this time, let's sort some array of strings in javascript
 
     let fruits = ["mango", "banana", "kiwi", "apple", "orange", "grape"];
 
     // now to sort this array of fruits, there's a built in method of arrays called "sort"
+    // this will sort the array of fruits to alphabetical order
     fruits = fruits.sort();
 
     // now if you want this to be in REVERSE alphabetical order, then you can do the method chaining
