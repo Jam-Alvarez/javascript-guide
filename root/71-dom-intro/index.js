b09ee52f9a478f38ba9fb1ab22bb29379a84b4f6
Jam-Alvarez <bc.jamiraquaimikhail.alvarez@cvsu.edu.ph>
2023-08-30 10:26:37 +0800

    71 DOM Intro Added

---
+++ root/71-dom-intro/index.js
@@ -0,0 +1,18 @@
+
+// DOM = Document Object Model (API or Application Programming Interface)
+// an interface for changing the content of the page
+
+console.log(document);
+console.dir(document);
+console.log(document.title);
+console.log(document.URL);
+
+document.title = "71 DOM Intro";
+// document.location = "http://www.google.com";
+
+document.body.style.backgroundColor = "lightblue";
+document.getElementById('js-div').innerHTML = "Hello!";
+
+
+
+// this is the DOM, and API
