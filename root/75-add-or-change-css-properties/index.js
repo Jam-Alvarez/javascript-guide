0bbc9ff1d288efad55111726ea34b16d2feb941c
Jam-Alvarez <bc.jamiraquaimikhail.alvarez@cvsu.edu.ph>
2023-08-31 13:47:47 +0800

    75 Add/Change CSS Properties Added

---
+++ root/75-add-or-change-css-properties/index.js
@@ -0,0 +1,13 @@
+
+const titleElement = document.getElementById('js-title');
+
+titleElement.style.backgroundColor = "black";
+titleElement.style.color = "white";
+titleElement.style.fontFamily = 'Cascadia Mono';
+titleElement.style.textAlign = "center";
+titleElement.style.border = "3px solid lightpink";
+titleElement.style.display = "block";
+
+
+
+// this is the very basic way to change CSS properties 
